20ac90f5f1faea9eaf98da548d9c7d4bee38ee33
dai-ski <adityaq0p@gmail.com>
2026-05-25 09:08:09 +0530

    feat: implement executeAppleScript utility function

--- Sources/Lyra/SpotifyBridge.swift
+++ Sources/Lyra/SpotifyBridge.swift
@@ -19,3 +19,30 @@ public struct SpotifyBridge: Sendable {
 
     public init() {}
 }
+
+extension SpotifyBridge {
+    @MainActor
+    private func executeAppleScript(_ source: String) throws -> String {
+        guard let script = NSAppleScript(source: source) else {
+            throw SpotifyBridgeError.appleScriptError(code: -1, message: "Could not initialize AppleScript.")
+        }
+
+        var errorInfo: NSDictionary? = nil
+        let descriptor = script.executeAndReturnError(&errorInfo)
+
+        if let errorInfo = errorInfo {
+            let code = (errorInfo[NSAppleScript.errorNumber] as? Int) ?? -1
+            let message = (errorInfo[NSAppleScript.errorMessage] as? String) ?? "Unknown AppleScript Error"
+            if code == -1743 {
+                throw SpotifyBridgeError.permissionDenied
+            }
+            throw SpotifyBridgeError.appleScriptError(code: code, message: message)
+        }
+
+        guard let stringValue = descriptor.stringValue else {
+            throw SpotifyBridgeError.invalidResponse
+        }
+
+        return stringValue
+    }
+}
